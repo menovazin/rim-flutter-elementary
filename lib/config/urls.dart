26ed846ed256b0195a52310ea472f4bdd14ddad1
 Denis Berezin <d.berezin@live.ru>
2026-07-14 16:01:51 +0300

    fix: update API base URL to include port and add new entries to .gitignore and Podfile.lock

--- lib/config/urls.dart
+++ lib/config/urls.dart
@@ -1,7 +1,7 @@
 class AppUrls {
   const AppUrls._();
 
-  static const String base = 'https://alpha.syazy.com/api';
+  static const String base = 'https://alpha.syazy.com:1180/api';
 
   static const bool useMock = bool.fromEnvironment('useMock');
 }
